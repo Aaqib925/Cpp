db295576343e204d7e2373c70f59a1b6c401bbc0
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-13 19:26:06 +0500

    working

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    // variables are the containers which holds the date and store it in the computer's memory
+    // in cpp we need to declare the variable by first defining it's date type
+    // int, float, char, double, float, string are some basic data types.
+
+}
