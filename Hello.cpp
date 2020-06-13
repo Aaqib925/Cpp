b0dfc284000fea52b929ecc502f73d408a88316d
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-13 17:55:55 +0500

    working

--- Hello.cpp
+++ Hello.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world";
-}
+    cout << "Hello world" << endl; // if you didn't added using namespace std, then to print 
+    // something to console(terminal) requires std::cout
+    // endl is used to terminate the line
+    cout << "Aaqib here";
+};
