885c343a8cc0d250da2e240c7db7028f9ee23e5e
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-14 17:10:16 +0500

    working

--- datatypes.cpp
+++ datatypes.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main(){
 
     // for character datatype we use the keyword char and use single quotations.
-    
+    // float and double are datatypes for point decimal values, one store 8 bytes and 4 bytes in the memory
+
+    float pointValue = 1.4;
+    cout << pointValue << endl;
 
 }
