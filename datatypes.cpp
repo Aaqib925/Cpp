fcc29e13e1cb01a6bed7cc2269bea42b5d8faaaa
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-14 17:36:10 +0500

    working

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// int main(){
 
     // for character datatype we use the keyword char and use single quotations.
     // float and double are datatypes for point decimal values, one store 8 bytes and 4 bytes in the memory
 
     // float pointValue = 1.4;
     // cout << pointValue << endl;
-    int x;
-    cout << "Enter any number: ";
+    // int x;
+    // cout << "Enter any number: ";
 
-    cin >> x;
-    if (x % 2 == 0){
-        cout << "The number " << x << " is Even number." << endl;
-    }
-    else{
-        cout << "The number " << x << " is Odd number." << endl;
+    // cin >> x;
+    // if (x % 2 == 0){
+    //     cout << "The number " << x << " is Even number." << endl;
+    // }
+    // else{
+    //     cout << "The number " << x << " is Odd number." << endl;
+    // }
+    // return 0;
+// }
+
+int main(){
+    for (int i = 0; i <= 25; i++){
+        cout << i << endl;
     }
     return 0;
 }
