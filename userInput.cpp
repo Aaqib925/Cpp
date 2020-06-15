ae1440e96fe2a87427865f9dbb32968f54f85715
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-16 01:40:24 +0500

    working

--- userInput.cpp
+++ userInput.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
+int main(){
+    // int a, b;
+
+    // cin >> a >> b;
+
+    // cout << a << " " << b;
+
+    // trying to get the float or double as input
+
+    // double num1;
+    // cin >> num1;
+    // cout << num1 << endl;
+
+    // double n1, n2;
+
+    // cout << fixed << showpoint << setprecision(3);
+    // cin >> n1 >> n2;
+    // cout << n1 << " " << n2 << endl;
+
+    // double n1, n2;
+
+    // cin >> n1 >> n2;
+    // cout << n1 << " " << n2 << endl;
+
+    // getline is another function to read input from user as a string
+
+    string x;
+    getline(cin, x);
+    cout << x << endl;
+
+    
+}
