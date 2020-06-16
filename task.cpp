86a64e1f749a670889ba4f6d669ba2131f57791d
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-16 22:39:42 +0500

    working

--- task.cpp
+++ task.cpp
@@ -23,10 +23,35 @@ using namespace std;
 //     return 0;
 // }
 
+// int main(){
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "Value of q" << " " << x / y << endl;
+//     cout << "Value of r" << " " << x % y << endl;
+    
+// }
+
+// euclideans's algo to find greatest common divisor
+
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << "Value of q" << x / y << endl;
-    cout << "Value of r" << x % y << endl;
+    int a, b, q, r, num1, num2;
+    cout << "Enter two numbers to find GCD: ";
+    cin >> num1 >> num2;
+    a = max(num1, num2);
+    b = min(num1, num2);
+    q = a / b;
+    r = a % b;
+    while (true){
+        a = b;
+        b = r;
+        q = a / b;
+        if (a % b != 0){
+            r = a % b;
+        }
+        else{
+            break;
+        }
+
+    }
     
 }
