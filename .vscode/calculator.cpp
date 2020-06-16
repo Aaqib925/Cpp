b800d6f564936aea688d8a6d01b7c3907c19472b
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-17 03:06:07 +0500

    working

---
+++ .vscode/calculator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+double addNumbers(double, double);
+double subNumbers(double, double);
+double divNumbers(double, double);
+double multNumbers(double, double);
+
+int main(){
+    double result;
+    int operations;
+    cout << "Enter number of Operations: ";
+    cin >> operations;
+
+    bool number = true;
+    bool operation = false;
+
+    for (int i = 0; i < operations; i++){
+        double secondValue;
+        string symbol;
+
+        if (number == true && operation == false){
+            cout << "Enter Operand number " << i + 1 << " : ";
+            cin >> secondValue;
+            number = false;
+            operation = true;
+        }
+
+        else if (number == false && operation == true){
+            cout << "Enter the operator: ";
+            cin >> symbol;
+            number = true;
+            operation = false;
+        }
+
+        if (i == 0){
+            result = secondValue;
+        }
+        else{
+            if (symbol == "+"){
+                
+            }
+        }
+
+
+    }
+
+}
