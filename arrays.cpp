58b5d584f049dbdacf52737c9e75544e198d977d
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-18 19:57:48 +0500

    working

---
+++ arrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // we specify the type of data used in the array with [index]
+
+    // in cpp we cannot print the array on console, we have to iterate through every element
+
+
+}
