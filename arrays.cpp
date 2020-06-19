e52af112e6d2cf212cf2cc15c54ed6ecf2d13e99
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-19 14:17:26 +0500

    done

--- arrays.cpp
+++ arrays.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main(){
     // we specify the type of data used in the array with [index]
 
     // in cpp we cannot print the array on console, we have to iterate through every element
 
-
+    string cars[4] = {"Mazda", "Corolla", "Civic", "Honda"};
+    cout << cars << endl; // we can't print like this
+    
 }
