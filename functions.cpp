15d63bd7201681d4c5a7bfd8914fe4c81ccef7d3
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-19 18:34:57 +0500

    working

--- functions.cpp
+++ functions.cpp
@@ -4,13 +4,23 @@ using namespace std;
 // void functions do not return anything
 // if you want to return any thing from the function, use the data type while declaring it
 void printName(string);
+
+int returnFunc(int arg1, int arg2);
+
+
 int main(){
     printName("Aaqib");
     printName("Abbas beemar");
     printName("Faiq also beemar");
 
+    cout << returnFunc(2, 4) << endl;
+
 }
 
 void printName(string arg1){
     cout << "Hello " << arg1 << endl;
+}
+
+int returnFunc(int arg1, int arg2){
+    return arg1 + arg2;
 }
