587368841cc51880a3e6b76ee4e9abff4663f9e5
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-19 15:01:52 +0500

    working

--- arrays.cpp
+++ arrays.cpp
@@ -7,6 +7,16 @@ int main(){
     // in cpp we cannot print the array on console, we have to iterate through every element
 
     string cars[4] = {"Mazda", "Corolla", "Civic", "Honda"};
-    cout << cars << endl; // we can't print like this
+    // cout << cars << endl; // we can't print like this
+    // you can access any element using it's index
+    // also you can assign the value for certain index
+    cars[1] = "Rolls Royce";
+    int arrSize = sizeof(cars)/sizeof(cars[0]); // this is method is get length or array
+    cout << arrSize << endl;
     
+    // int arrlength = cars[1].find('R');
+    // cout << arrlength << endl;
+
+    // int arrlength = cars.find(cars[-1]);
+    // cout << arrlength;
 }
