5dd591820c612bd6fd793a72d5ddd329586709e1
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-19 20:41:44 +0500

    working

--- task.cpp
+++ task.cpp
@@ -88,37 +88,42 @@ using namespace std;
 
 // hackerrank challenge
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int n, q;
-    cin >> n >> q;
-    int firstLength, secondLength;
-    cin >> firstLength;
-    // first array
-    int firstArray[firstLength], secondArray[secondLength];
-    int x;
-    for (int i = 0; i < firstLength; i++){
-        cin >> x;
-        firstArray[i] = x;
-        // cout << x << endl;
-    }
-    
-    cin >> secondLength;
-    for (int i = 0; i < secondLength; i ++){
-        cin >> x;
-        secondArray[i] = x;
-    }
-    int arrayNumber, j;
-    for (int i = 0; i < q; i ++){
-        cin >> arrayNumber >> j;
-        if (arrayNumber == 0){
-            cout << firstArray[j] << endl;
-        }
-        else if (arrayNumber == 1){
-            cout << secondArray[j] << endl;
+// int main() {
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+//     int n, q;
+//     cin >> n >> q;
+//     int firstLength, secondLength;
+//     cin >> firstLength;
+//     // first array
+//     int firstArray[firstLength], secondArray[secondLength];
+//     int x;
+//     for (int i = 0; i < firstLength; i++){
+//         cin >> x;
+//         firstArray[i] = x;
+//         // cout << x << " ";
+//     }
+//     // cout << endl;
 
-        }
-    }
-    
-    return 0;
-}
+//     cin >> secondLength;
+//     int y;
+//     for (int i = 0; i < secondLength; i ++){
+//         cin >> y;
+//         secondArray[i] = y;
+//         // cout << y << " ";
+//     }
+//     int arrayNumber, index;
+//     for (int a = 0; a < q; a ++){
+//         cin >> arrayNumber >> index;
+//         // cout << arrayNumber << " " << j << endl;
+//         if (arrayNumber == 0){
+//             cout << firstArray[index] << endl;
+
+//         }
+//         else if (arrayNumber == 1){
+//             cout << secondArray[index] << endl;
+//         }
+//     }
+// }
+
+// 10 10 3 916135 272188 794963 3 178997 502468 671251 1 122266 3 223511 996043 990104 3 319694 335208 200789 2 867809 273793 1 925358 1 71140 1 862238 1 994309 6 0 5 0 5 0 7 0 5 0 6 0 3 2 3 1 0 0 9 0
+
