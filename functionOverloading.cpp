a29239368ebdc8d1006f39216686af65cf3f43c7
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-22 14:59:13 +0500

    working

--- functionOverloading.cpp
+++ functionOverloading.cpp
@@ -2,7 +2,17 @@
 #include <iomanip>
 using namespace std;
 
-int square(int);
+int square(int);   // the function square is overloading with different argument attributes
 double square(double);
 
+void main(){
+    int userInt;
+    double userDouble;
+
+    cout << fixed << showpoint << setprecision(2);
+    cout << "Enter an Integer and Float Value: ";
+    cin >> userInt >> userDouble;
+    
+
+}
 
