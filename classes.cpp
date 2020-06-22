6ace1e716e4080b1e2c4b920defe130b981ce332
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-22 14:54:49 +0500

    working

--- classes.cpp
+++ classes.cpp
@@ -12,9 +12,14 @@ class myClass{
     void showData(){
         cout << "Data is: " << somedata << endl;
     }
-}
+};
 
 int main(){
+    // to make instance for class use classname with instance name
 
-    
+    myClass obj1, obj2;
+    obj1.setData(4);
+    obj2.setData(5);
+    obj1.showData();
+    obj2.showData();
 }
