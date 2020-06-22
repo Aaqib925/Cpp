b6ad4085dd3704c67c53105567c61a23afbce011
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-22 21:28:07 +0500

    working

--- loops.cpp
+++ loops.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int x = 1;
-    while (x < 5){
-        cout << x << endl;
-        x += 1;
-    }
+// int main(){
+//     int x = 1;
+//     while (x < 5){
+//         cout << x << endl;
+//         x += 1;
+//     }
 
-    do{ // it executes first then check the condition
-        cout << x << endl;
-        x += 1;
+//     do{ // it executes first then check the condition
+//         cout << x << endl;
+//         x += 1;
+//     }
+//     while(x <= 10);
+// }
+
+// using for loop
+
+int main(){
+    for (int i = 0; i < 10; i ++){
+        cout << i << endl;
     }
-    while(x <= 10);
 }
