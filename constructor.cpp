7bb76be35b09951423049d503eb1b4f42253a4d4
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-22 18:13:03 +0500

    working

---
+++ constructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Counter{
+    private:
+    int count;
+
+    public:
+    Counter(){    // this is default constructor
+        cout << "Created an object." << endl;
+        count = 1;
+        cout << "The Value of count is set to one." << endl;
+    }
+
+}
