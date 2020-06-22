19eb081ad8c4f5272d5a30e4391f318f7c1bce78
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-22 21:19:27 +0500

    working

--- switch.cpp
+++ switch.cpp
@@ -8,6 +8,21 @@ int main(){
 
     string DayName;
 
+    switch (x)
+    {
+    case 0: DayName = "Sunday"; break;
+    case 1: DayName = "Monday"; break;
+    case 2: DayName = "Tuesday"; break;
+    case 3: DayName = "Wednesday"; break;
+    case 4: DayName = "Thursday"; break;
+    case 5: DayName = "Friday"; break;
+    case 6: DayName = "Saturday"; break;
+
     
+    default:
+        cout << "You Entered a Invalid Number."
+        break;
+    }
+
 
 }
