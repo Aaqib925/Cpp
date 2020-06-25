0303bf632440451e1a8b8959cdf8cef50fdf1f03
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-26 00:50:46 +0500

    working

--- task.cpp
+++ task.cpp
@@ -494,6 +494,10 @@ int main() {
             }
             
         }
-        cout << s1 << " " << s2;
+        cout << s1.length() << endl << s2.length() << endl;
+        cout << s1 + s2 << endl;
+        swap(s1[0], s2[0]);
+        cout << s1 << "\n" << s2;
+
     }
 }
