7ac457fd76da5631863aab602cf5f81d1cacdc3d
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-27 02:10:15 +0500

    working

---
+++ pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age = 15;
+    double gpa = 2.5;
+    string name = "Aaqib";
+
+    // print the memory of variable we use ampersand sign
+
+    cout << &age << endl;
+    // we can pass this memory address and can use it as an object
+    
+}
