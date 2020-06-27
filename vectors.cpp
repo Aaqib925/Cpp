374ca61ba6873a518861106d37219c0c87c14041
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-28 02:13:57 +0500

    working

---
+++ vectors.cpp
@@ -0,0 +1,18 @@
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> myVector;
+//     for (int i = 0; i < n; i++){
+//         int x;
+//         cin >> x;
+//         myVector.push_back(x);
+        
+//     }
+//     sort(myVector.begin(), myVector.end());
+    
+    
+//     for (int i = 0; i < n; i++){
+//         cout << myVector[i] << " ";
+//     }
+
+// }
