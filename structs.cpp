0bf086c2d2199123ee0a27a6ea787275a62f287b
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-28 00:55:16 +0500

    working

--- structs.cpp
+++ structs.cpp
@@ -12,5 +12,17 @@ struct info{
 
 int main(){
 
+    // creating instance of struct
+    // first right struct name then the variable name
+
+    info firstPerson;
+    firstPerson = {
+        "Aaqib",
+        18,
+        2.7,
+        "Nazir"
+    };
+
+    cout << firstPerson.age << endl;
 
 }
