ca7d9671273964b54e58c526951ac1b1ecc5969b
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-28 01:59:17 +0500

    working

--- task.cpp
+++ task.cpp
@@ -674,21 +674,21 @@ using namespace std;
 
 // };
 
-int main(){
-    int n;
-    cin >> n;
-    vector<int> myVector;
-    for (int i = 0; i < n; i++){
-        int x;
-        cin >> x;
-        myVector.push_back(x);
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> myVector;
+//     for (int i = 0; i < n; i++){
+//         int x;
+//         cin >> x;
+//         myVector.push_back(x);
         
-    }
-    sort(myVector.begin(), myVector.end());
+//     }
+//     sort(myVector.begin(), myVector.end());
     
     
-    for (int i = 0; i < n; i++){
-        cout << myVector[i] << " ";
-    }
+//     for (int i = 0; i < n; i++){
+//         cout << myVector[i] << " ";
+//     }
 
-}
+// }
