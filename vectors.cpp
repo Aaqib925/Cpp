4ecedc43a531372ca33e76065d09e25153146b2a
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-28 02:39:20 +0500

    working

--- vectors.cpp
+++ vectors.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 
@@ -28,29 +30,50 @@ using namespace std;
 // Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
 // Ex:v.erase(v.begin()+2,v.begin()+5);(erases all the elements from the third element to the fifth element.)
 
-int main(){
-    int n, e;
-    cin >> n;
-    vector<int> myVector;
+// int main(){
+//     int n, e;
+//     cin >> n;
+//     vector<int> myVector;
 
-    for (int i = 0; i < n; i ++){
+//     for (int i = 0; i < n; i ++){
+//         int x;
+//         cin >> x;
+//         myVector.push_back(x);
+//     }
+//     cin >> n;
+
+//     myVector.erase(myVector.begin() + n);
+
+//     cin >> n >> e;
+
+//     myVector.erase(myVector.begin() + n - 1, myVector.begin() + e - 1);
+
+//     cout << myVector.size() << endl;
+
+//     for (int i = 0; i < myVector.size(); i++){
+//         cout << myVector[i] << " ";
+//     }
+
+
+// }
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+
+    for (int i = 0; i < n; i++){
         int x;
         cin >> x;
-        myVector.push_back(x);
-    }
-    cin >> n;
-
-    myVector.erase(myVector.begin() + n);
-
-    cin >> n >> e;
-
-    myVector.erase(myVector.begin() + n - 1, myVector.begin() + e - 1);
-
-    cout << myVector.size() << endl;
-
-    for (int i = 0; i < myVector.size(); i++){
-        cout << myVector[i] << " ";
+        v.push_back(x);
     }
 
+    int q;
+    cin >> q;
+    for(int i = 0; i < q; i ++){
+        int value;
+        cin >> value;
 
+        
+    }
 }
