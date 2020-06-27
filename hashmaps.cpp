d1d6d04247662700556bd3d1f969ad90b773638c
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-27 18:47:18 +0500

    working

--- hashmaps.cpp
+++ hashmaps.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <map>
 using namespace std;
  
 int main(){
     map<string, int> myHashMap;
     // first provide data type for key and then for the value
+    myHashMap["Hello"] = 24;
+    myHashMap["Aaqib"] = 18;
+
+    if (myHashMap.find("World") == myHashMap.end()){
+        cout << "The key world is not present." << endl;
+    } 
+    cout << myHashMap["world"] << endl; << // if key is not present in hashmap, it result 0 value
+
+    
 }
