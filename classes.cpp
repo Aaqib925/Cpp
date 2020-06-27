eed9f1099c569e1e260cb958d93a2901e01f9649
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-28 01:14:52 +0500

    working

--- classes.cpp
+++ classes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 class myClass{
@@ -23,3 +24,53 @@ int main(){
     obj1.showData();
     obj2.showData();
 }
+
+class Student{
+    private:
+    int age, standard;
+    string fname, lname;
+    public:
+    Student(){
+        int age = 0;
+        int standard = 0;
+        fname = "";
+        lname = "";
+    }
+    // get_age, set_age
+    // get_first_name, set_first_name
+    // get_last_name, set_last_name
+    // get_standard, set_standard
+    void set_age(int userAge){
+        age = userAge;
+    }
+    void set_first_name(string userName){
+        fname = userName;
+    }
+    void set_last_name(string userLastName){
+        lname = userLastName;
+    }
+    void set_standard(int userStandard){
+        standard = userStandard;
+    }
+    // getters
+
+    int get_age(){
+        return age;
+    }
+
+    int get_standard(){
+        return standard;
+    }
+    string get_first_name(){
+        return fname;
+    }
+    string get_last_name(){
+        return lname;
+    }
+    string to_string(){;
+        stringstream ss;
+        char c = ',';
+        ss << age << c << fname << c << lname << c << standard;
+        return ss.str();
+    }
+};
