ca96945eaa71afb85cee2841d19b3fbaec9940a6
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-30 19:26:56 +0500

    working'

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Chef{
+    public:
     void makeChicken(){
         cout << "Chef is making chicken.";
     }
@@ -9,4 +10,21 @@ class Chef{
     void makeSpecialDish(){
         cout << "Chef is making Special Dish";
     }
+};
+
+class AnotherChef: public Chef{
+    public:
+    void makePasta(){
+        cout << "Another Chef is making Pasta";
+    }
+};
+
+int main(){
+    Chef chef1;
+    chef1.makeChicken();
+
+    AnotherChef chef2;
+    chef2.makeChicken();
+    chef2.makePasta();
+
 }
