be8ff5f4c006152a437e79068d19fd5b8561480c
aaqib925 <muhammadaaqib925@gmail.com>
2020-06-30 19:20:17 +0500

    working'

---
+++ inheritance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Chef{
+    void makeChicken(){
+        cout << "Chef is making chicken.";
+    }
+
+    void makeSpecialDish(){
+        cout << "Chef is making Special Dish";
+    }
+}
