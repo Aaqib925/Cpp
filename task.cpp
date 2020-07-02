b42e3062ae22ea24c3ea72b4e462648e42395044
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-03 01:24:06 +0500

    wirking

--- task.cpp
+++ task.cpp
@@ -1211,30 +1211,50 @@ using namespace std;
 // }
 
 // input: 1 100.345 2006.008 2331.41592653498
-int main() {
-	int T; cin >> T;
-	cout << setiosflags(ios::uppercase);
-	cout << setw(0xf) << internal;
-	while(T--) {
-		double A; cin >> A;
-		double B; cin >> B;
-		double C; cin >> C;
+// int main() {
+// 	int T; cin >> T;
+// 	cout << setiosflags(ios::uppercase);
+// 	cout << setw(0xf) << internal;
+// 	while(T--) {
+// 		double A; cin >> A;
+// 		double B; cin >> B;
+// 		double C; cin >> C;
 
-		/* Enter your code here */
-        // for first line
-        cout << hex << left << showbase << nouppercase;
-        cout << (long long)A << endl;
+// 		/* Enter your code here */
+//         // for first line
+//         cout << hex << left << showbase << nouppercase;
+//         cout << (long long)A << endl;
 
-        // for second line
+//         // for second line
 
-        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2);
-        cout << B << endl;
+//         cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2);
+//         cout << B << endl;
 
-        // for third line
+//         // for third line
 
-        cout << scientific << uppercase << noshowpos << setprecision(9);
-        cout << C << endl;
-	}
-	return 0;
+//         cout << scientific << uppercase << noshowpos << setprecision(9);
+//         cout << C << endl;
+// 	}
+// 	return 0;
 
-}
+// }
+
+// Printed Line 1
+
+// The manipulators I used here are hex (which will output the number in hexadecimal format), left (which will align the number to the left), showbase (which will make sure the hexadecimal number has a '0x' at the beginning), and nouppercase (which converts all alphabetic hexadecimal values to lowercase).
+
+// Printed Line 2 In this section, I actually have to undo some of the previous manipulations.
+
+// dec: switches numbers from hexadecimal back to decimal.
+// right: aligns values to the right instead of the left
+// setw(15): sets a fixed width of 15, as the effect from the initial code only impacts the first printed line.
+// setfill('_'): by default, when you have a fixed width, if your printed value doesn't fill up the entire length (for example, if you have a width of 15 and only print 7 characters), the extra characters used to pad are whitespaces. This function lets you change the padding to whatever character you want.
+// showpos: Makes sure there is a plus sign before any positive numbers
+// fixed: ensures that number is printed out entirely and that scientific notation isn't used for larger numbers
+// setprecision(2): sets the number of decimal places to 2.
+// Printed Line 3
+
+// scientific: Prints output in scientific notation format
+// uppercase: Undoes previous nouppercase manipulator and ensures that the 'E' in the scientific notation is capitalised
+// noshowpos: Undoes previous showpos manipulator and gets rid of the plus at the start of positive values
+// setprecision: Changes the number of digits after the decimal place from 2 to 9.
