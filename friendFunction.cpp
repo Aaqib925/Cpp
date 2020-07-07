fc1afc9f2f1ed5b0fa704806ef9db6acefaf0266
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-07 22:39:36 +0500

    working

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 
 using namespace std;
+
+// friend function is not a member function of class but it friend of class
+// it is declared inside the class, but define outside the class
+
+class Complex{
+    private:
+    int x, y;
+
+    public:
+    void setData(){
+        cin >> x >> y;
+    }
+
+    void showdata(){
+        cout << x << " "<< y;
+    }
+
+    friend void sum(Complex);
+
+};
+
+void sum(Complex c){
+    cout << c.x + c.y << endl;
+    
+}
+
+int main(){
+    Complex c1, c2;
+    c1.setData();
+    c2.setData();
+
+    sum(c1);
+    sum(c2);
+
+}
