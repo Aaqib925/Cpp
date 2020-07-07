dea08919abacc5ffba42609b73c74aaf20c9227b
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-07 16:45:27 +0500

    working

--- task.cpp
+++ task.cpp
@@ -1286,33 +1286,33 @@ using namespace std;
 // };
 
 
-class Triangle{
-	public:
-		void triangle(){
-			cout<<"I am a triangle\n";
-		}
-};
+// class Triangle{
+// 	public:
+// 		void triangle(){
+// 			cout<<"I am a triangle\n";
+// 		}
+// };
 
-class Isosceles : public Triangle{
-  	public:
-  		void isosceles(){
-    		cout<<"I am an isosceles triangle\n";
-  		}
-};
+// class Isosceles : public Triangle{
+//   	public:
+//   		void isosceles(){
+//     		cout<<"I am an isosceles triangle\n";
+//   		}
+// };
 
-//Write your code here.
-class Equilateral: public Isosceles{
-    public:
-    void equilateral(){
-        cout << "I am an equilateral triangle" << endl;
-    }
+// //Write your code here.
+// class Equilateral: public Isosceles{
+//     public:
+//     void equilateral(){
+//         cout << "I am an equilateral triangle" << endl;
+//     }
 
-};
-int main(){
+// };
+// int main(){
   
-    Equilateral eqr;
-    eqr.equilateral();
-    eqr.isosceles();
-    eqr.triangle();
-    return 0;
-}
+//     Equilateral eqr;
+//     eqr.equilateral();
+//     eqr.isosceles();
+//     eqr.triangle();
+//     return 0;
+// }
