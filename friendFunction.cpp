62c13bd3b288af355e933a207e0ce18fb82c6868
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-07 22:48:23 +0500

    working

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -5,34 +5,67 @@ using namespace std;
 // friend function is not a member function of class but it friend of class
 // it is declared inside the class, but define outside the class
 
-class Complex{
+// class Complex{
+//     private:
+//     int x, y;
+
+//     public:
+//     void setData(){
+//         cin >> x >> y;
+//     }
+
+//     void showdata(){
+//         cout << x << " "<< y;
+//     }
+
+//     friend void sum(Complex);
+//     // a friend function can access 2 or more classes at the same time
+
+// };
+
+// void sum(Complex c){
+//     cout << c.x + c.y << endl;
+    
+// }
+
+// int main(){
+//     Complex c1, c2;
+//     c1.setData();
+//     c2.setData();
+
+//     sum(c1);
+//     sum(c2);
+
+// }
+
+class B;
+class A{
     private:
-    int x, y;
+    int x = 5;
 
     public:
-    void setData(){
-        cin >> x >> y;
-    }
 
-    void showdata(){
-        cout << x << " "<< y;
-    }
-
-    friend void sum(Complex);
+    friend void myfun(A, B);
 
 };
 
-void sum(Complex c){
-    cout << c.x + c.y << endl;
+class B{
+    private:
+    int x = 5;
+
+    public:
     
+    friend void myfun(A, B);
+
+};
+
+void myfun(A obj1, B obj2){
+    cout << obj1.x + obj2.x << endl;
 }
 
 int main(){
-    Complex c1, c2;
-    c1.setData();
-    c2.setData();
+    A a1;
+    B b1;
 
-    sum(c1);
-    sum(c2);
-
+    myfun(a1, b1);
 }
