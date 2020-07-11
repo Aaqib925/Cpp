ca94e4f7c9cd20724328c368a0bbeef590fbec2e
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-11 21:11:30 +0500

    working

--- pointers.cpp
+++ pointers.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// int main(){
 
-    int age = 15;
-    double gpa = 2.5;
-    string name = "Aaqib";
+//     int age = 15;
+//     double gpa = 2.5;
+//     string name = "Aaqib";
 
-    // print the memory of variable we use ampersand sign
+//     // print the memory of variable we use ampersand sign
 
-    cout << &age << endl;
-    // we can pass this memory address and can use it as an object
-    // store the address in a variable we use * sign
+//     cout << &age << endl;
+//     // we can pass this memory address and can use it as an object
+//     // store the address in a variable we use * sign
 
-    int *pAge = &age;
-    cout << pAge << endl;
+//     int *pAge = &age;
+//     cout << pAge << endl;
 
-    // we can dereference the pointer to get the true value
+//     // we can dereference the pointer to get the true value
 
-    cout << *pAge << endl;
-}
+//     cout << *pAge << endl;
+// }
+
+class Box{
+    private:
+    int l, b, h;
+
+    public:
+
+    void setDimension(int x, int y, int z){
+        l = b;
+        b = y;
+        h = z;
+    }
+
+    void showData(){
+        cout << l << " " << b << " " << h << endl;
+    }
+
+};
+
+
