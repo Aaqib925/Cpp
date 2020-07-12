2905c2aebed2a67dfd6485af068e981b14f96ca9
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-13 00:46:07 +0500

    working

--- deque.cpp
+++ deque.cpp
@@ -4,4 +4,11 @@ using namespace std;
 
 int main(){
     deque<int> mydeque; //Creates a double ended queue of deque of int type
+
+    int length = mydeque.size(); //Gives the size of the deque
+
+    mydeque.pop_back(); //Pops element from the end
+    mydeque.pop_front(); //Pops element from the beginning
+
+    cout << mydeque.empty() << endl; //Returns a boolean value which tells whether the deque is empty or not
 }
