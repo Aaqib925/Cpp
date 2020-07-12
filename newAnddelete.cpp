775e6306522106b01b066686f201129e66fdd2cf
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-12 15:15:45 +0500

    working

--- newAnddelete.cpp
+++ newAnddelete.cpp
@@ -5,6 +5,9 @@ int main(){
     // new function is used to create DMA variable, dynamic memory allocations
     // To allocate the memory by the programmer for the variable....
     // whereas it only contains the address
+    // this is allocated on Heap memory, whereas simple static memory allocation has
+    // stack memory type
+
 
     int *p = new int;
     cout << p << endl;
