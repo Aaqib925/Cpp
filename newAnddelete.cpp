0982c2411f4b7d7a18877632cfdec15726117bae
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-12 15:33:07 +0500

    working

--- newAnddelete.cpp
+++ newAnddelete.cpp
@@ -20,5 +20,5 @@ int main(){
 
     delete p; // P is not deleted, The thing which is being pointed is deleted
 
-    delete []array;
+    delete []array; // for deleting an array we use brackets
 }
