b99dab6e22b8315c1c2ed664d3bfa8c67f830931
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-13 17:19:51 +0500

    working

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -197,59 +197,68 @@ using namespace std;
 
 // Review Question
 
-class Complex{
-    public:
-    int x;
-    int y;
+// class Complex{
+//     public:
+//     int x;
+//     int y;
 
-    Complex(){
-        x = 0; y = 0;
-    }
+//     Complex(){
+//         x = 0; y = 0;
+//     }
 
-    Complex(int x, int y){
-        this->x = x;
-        this->y = y;
-    }
+//     Complex(int x, int y){
+//         this->x = x;
+//         this->y = y;
+//     }
 
-    friend istream& operator>>(istream &din, Complex&);
-    friend ostream& operator<<(istream &dout, Complex);
+//     friend istream& operator>>(istream &din, Complex&); // overloading insertion operator
+//     friend ostream& operator<<(istream &dout, Complex); // overloading extraction operator
 
-    friend Complex operator+(Complex, Complex);
-    friend Complex operator-(Complex, Complex);
+//     friend Complex operator+(Complex, Complex);
+//     friend Complex operator-(Complex, Complex);
 
-};
+// };
 
-istream& operator>>(istream &din, Complex &C){
-    cin >> C.x >> C.y;
-    return din;
-}
+// istream& operator>>(istream &din, Complex &C){
+//     cout << "Enter Value of x: ";
+//     cin >> C.x;
+//     cout << "Enter value of y: ";
+//     cin >> C.y;
+//     return din;
+// }
 
-ostream& operator<<(ostream& dout, Complex C){
-    cout << C.x << "+" << C.y << "i" << endl;
-    return dout;
-}
+// ostream& operator<<(ostream& dout, Complex C){
+//     cout << C.x << "+" << C.y << "i" << endl;
+//     return dout;
+// }
 
-Complex operator+(Complex c1, Complex c2){
-    Complex temp;
-    temp.x = c1.x + c2.x;
-    temp.y = c1.y + c2.y;
+// Complex operator+(Complex c1, Complex c2){
+//     Complex temp;
+//     temp.x = c1.x + c2.x;
+//     temp.y = c1.y + c2.y;
 
-    return temp;
-}
+//     return temp;
+// }
 
-Complex operator-(Complex c1, Complex c2){
-    Complex temp;
-    temp.x = c1.x - c2.x;
-    temp.y = c1.y - c2.y;
+// Complex operator-(Complex c1, Complex c2){
+//     Complex temp;
+//     temp.x = c1.x - c2.x;
+//     temp.y = c1.y - c2.y;
 
-    return temp;
-}
+//     return temp;
+// }
 
-int main(){
-    Complex c1(2, 5);
-    cout << c1;
+// int main(){
+//     Complex c1(2, 5);
+//     cout << c1;
 
-    Complex c2;
-    cin >> c2;
-    cout << c2;
-}
+//     Complex c2, c3, c4;
+//     cin >> c2;
+//     cout << c2;
+
+//     c3 = c1 + c2;
+//     cout << c3;
+
+//     c4 = c1 - c2;
+//     cout << c4;
+// }
