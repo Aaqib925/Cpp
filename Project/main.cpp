dea0200928b7e0a6983c9ad7d04765ca97f8959a
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-15 18:45:31 +0500

    working

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <fstream>
+
 
 using namespace std;
 
+class User{
+    protected:
+    string username, password;
+    
+    public:
+
+    virtual void getData(){
+        cout << "Enter Username: ";
+        cin >> username;
+        cout << "Enter Password: ";
+        cin >> password;
+    }
+
+    void createAccount(string userData){
+        ofstream fout; // object for writing into the file
+
+        fout.open("Users.txt", ios::app); // opening the file in apppend mode
+
+        
+
+    }
+
+    virtual bool checkData(string userData){
+        
+    }
+
+
+};
 
 
 int main(){
