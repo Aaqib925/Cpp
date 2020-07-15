90e1c16226291f701373a235264a6131a32699f3
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-15 18:52:57 +0500

    working

--- Project/main.cpp
+++ Project/main.cpp
@@ -22,12 +22,34 @@ class User{
 
         fout.open("Users.txt", ios::app); // opening the file in apppend mode
 
-        
+        fout << userData << "\n";
+
+        fout.close();
 
     }
 
-    virtual bool checkData(string userData){
-        
+    bool checkData(string userData){
+        int offset;
+        string line;
+        ifstream fin;
+
+        fin.open("Users.txt");
+
+        if (fin.is_open()){
+
+            while(!fin.eof()){
+
+                getline(fin, line);
+                if(offset = line.find(userData, 0) != string::npos){
+                    cout << "FOUND" << endl;
+                    return true;
+                }
+                else{
+                    cout << "Not Fount" << endl;
+                    return false;
+                }
+            }
+        }
     }
 
 
