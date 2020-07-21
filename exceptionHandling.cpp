5ff755884e2717b25e922ed3de769efc0ae13de0
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-21 21:52:18 +0500

    ConversionsDone

---
+++ exceptionHandling.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+// There are three keywords, try, throw and catch, whenever the code in try block has an excecption
+// it throws the exception and throw block is executed, then catch block is used
+
