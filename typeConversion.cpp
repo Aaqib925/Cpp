1a8da81c3ddada1a99c6b4602a0e8fe2b038d691
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-21 20:41:18 +0500

    working

--- typeConversion.cpp
+++ typeConversion.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // Primitive data types are predefined in C++, whereas the classes we create are called as
 // non primitive data types, We can easily convert the primitive data type into primitive
 
-// Primitive type of class type.
+// Primitive type to class type.
 // Class to primitive
 // Class to class
 
@@ -29,9 +29,20 @@ class Complex{
     void showData(){
         cout << "x = " << x << " y = " << y << endl;
     }
+    // class to primitive
+    // we will use the operator function, along with the primitive data type to which we want
+    // to convert
+    operator int(){
+        return x + y;
+    }
 };
 
+
+
+
 int main(){
+
+    // primtive to class
     Complex c1;
     c1.getData(2, 5);
 
@@ -39,4 +50,10 @@ int main(){
     c1 = x;  // x value will be send as argument of constructor c1
 
     c1.showData();
+
+    // class to primitive
+
+    int y;
+    y = c1; // c1.operator int() called here, and y will be assigned according to what we defined.
+    cout << "The Value of y is: " << y << endl;
 }
