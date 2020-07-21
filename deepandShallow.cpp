9fdcafd874a427519a3bf2316f2133de115aa34a
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-21 17:34:23 +0500

    working

---
+++ deepandShallow.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+// copying the data of one object into the another using the copy constructor, from variable 
+// to another variable is called shallow copy. It doesn't copies the recources on which the object
+// is pointing outside the object, or dynamic block
