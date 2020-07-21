fb2d0fe515346e7093ed1a6d96dd05adae088a16
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-21 22:34:43 +0500

    ConversionsDone

--- exceptionHandling.cpp
+++ exceptionHandling.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 // There are three keywords, try, throw and catch, whenever the code in try block has an excecption
-// it throws the exception and throw block is executed, then catch block is used
+// then catch block is used
+// throw is used inside the try block
 
+int main(){
+    cout << "Welcome" << endl;
+    try{
+        throw 10;   // this is can be used for catch block, the data type is necessary
+                    // to be same for exception block
+        cout << "Hello" << endl; // this line will never get executed, because exception has been thrown above
+
+    }
+    // if we want the catch block to run for every exception we use ... int catch argument
+    
+    catch(int e){   // data type should be same as in try block
+        cout << "Error number is: " << e << endl;
+    }
+    cout << "Last Line" << endl;
+}
