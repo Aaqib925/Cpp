6dc73f12ce03e6171dc0641a298cbeeeb940254a
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-26 16:31:20 +0500

    working

--- task.cpp
+++ task.cpp
@@ -1995,3 +1995,21 @@ using namespace std;
 //         cout << "Key: " << element.first << " Value: " << element.second << endl;
 //     }
 // }
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
