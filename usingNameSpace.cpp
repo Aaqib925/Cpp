fa1ab59dd7540d539057f64c4f1150e1a13f97be
aaqib925 <muhammadaaqib925@gmail.com>
2020-07-30 09:54:23 +0500

    working

---
+++ usingNameSpace.cpp
@@ -0,0 +1,10 @@
+#include "mynamespace"   // for the headerfile inside the present directory we use ""
+using namespace mynamespace;
+
+int main(){
+    a = 5;
+    cout << a << endl;
+    f1();
+    A obj1;
+    obj1.fun();
+}
