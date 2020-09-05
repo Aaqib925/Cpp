0debf7efd225eb5c1b81ee8686b8843ada450a8a
aaqib925 <muhammadaaqib925@gmail.com>
2020-09-05 16:02:39 +0500

    working

--- task.cpp
+++ task.cpp
@@ -2976,83 +2976,84 @@ using namespace std;
 //     return 0;
 // }
 
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// class Message {
-//     private:
-//     string text;
-//     static int textId;
+class Message {
+    private:
+    string text;
+    static int id;
+    int currentId;
 
-// public: 
-//     Message() {textId += 1}
-//     Message(string text) {this->text = text; textId += 1}
-//     const string& get_text() {
-//         return text;
-//     }
-//     bool operator <(const Message& m)
-//     {
-//         if (textId < m.textId)
-//             return true;
-//         else
-//             return false;
-//     }
-// };
-// int Message::textId = 0;
+public: 
+    Message() {currentId = ++id;}
+    Message(string text) {this->text = text; currentId = ++id;}
+    const string& get_text() {
+        return text;
+    }
+    bool operator <(const Message& m)
+    {
+        if (currentId < m.currentId)
+            return true;
+        else
+            return false;
+    }
+};
+int Message::id = 0;
 
-// class MessageFactory {
-// public:
-//     MessageFactory() {}
-//     Message create_message(const string& text) {
-//         return Message(text);
-//     }
-// };
+class MessageFactory {
+public:
+    MessageFactory() {}
+    Message create_message(const string& text) {
+        return Message(text);
+    }
+};
 
-// class Recipient {
-// public:
-//     Recipient() {}
-//     void receive(const Message& msg) {
-//         messages_.push_back(msg);
-//     }
-//     void print_messages() {
-//         fix_order();
-//         for (auto& msg : messages_) {
-//             cout << msg.get_text() << endl;
-//         }
-//         messages_.clear();
-//     }
-// private:
-//     void fix_order() {
-//         sort(messages_.begin(), messages_.end());
-//     }
-//     vector<Message> messages_;
-// };
+class Recipient {
+public:
+    Recipient() {}
+    void receive(const Message& msg) {
+        messages_.push_back(msg);
+    }
+    void print_messages() {
+        fix_order();
+        for (auto& msg : messages_) {
+            cout << msg.get_text() << endl;
+        }
+        messages_.clear();
+    }
+private:
+    void fix_order() {
+        sort(messages_.begin(), messages_.end());
+    }
+    vector<Message> messages_;
+};
 
-// class Network {
-// public:
-//     static void send_messages(vector<Message> messages, Recipient& recipient) {
-//     // simulates the unpredictable network, where sent messages might arrive in unspecified order
-//         random_shuffle(messages.begin(), messages.end());         
-//         for (auto msg : messages) {
-//             recipient.receive(msg);
-//         }
-//     }
-// };
+class Network {
+public:
+    static void send_messages(vector<Message> messages, Recipient& recipient) {
+    // simulates the unpredictable network, where sent messages might arrive in unspecified order
+        random_shuffle(messages.begin(), messages.end());         
+        for (auto msg : messages) {
+            recipient.receive(msg);
+        }
+    }
+};
 
 
 
-// int main() {
-//     MessageFactory message_factory;
-//     Recipient recipient;
-//     vector<Message> messages;
-//     string text;
-//     while (getline(cin, text)) {
-//         messages.push_back(message_factory.create_message(text));
-//     }
-//     Network::send_messages(messages, recipient);
-//     recipient.print_messages();
-// }
+int main() {
+    MessageFactory message_factory;
+    Recipient recipient;
+    vector<Message> messages;
+    string text;
+    while (getline(cin, text)) {
+        messages.push_back(message_factory.create_message(text));
+    }
+    Network::send_messages(messages, recipient);
+    recipient.print_messages();
+}
 
