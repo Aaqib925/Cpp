1530cbd8899b8576db05c58742d09e5e6406179b
aaqib925 <muhammadaaqib925@gmail.com>
2020-09-11 01:31:43 +0500

    working

--- task.cpp
+++ task.cpp
@@ -3990,19 +3990,88 @@ using namespace std;
 //     return 0;
 // }
 // Complete the insertionSort function below.
-int insertionSort(vector<int> arr) {
-    int i, key, j, count;  
-    for (i = 1; i < arr.size(); i++) 
-    {  
-        key = arr[i];  
-        j = i - 1;  
-        while (j >= 0 && arr[j] > key) 
-        {  
-            arr[j + 1] = arr[j];  
-            j = j - 1;
-            count += 1;  
-        }  
-        arr[j + 1] = key;  
+// int insertionSort(vector<int> arr) {
+//     int i, key, j, count;  
+//     for (i = 1; i < arr.size(); i++) 
+//     {  
+//         key = arr[i];  
+//         j = i - 1;  
+//         while (j >= 0 && arr[j] > key) 
+//         {  
+//             arr[j + 1] = arr[j];  
+//             j = j - 1;
+//             count += 1;  
+//         }  
+//         arr[j + 1] = key;  
+//     }
+//     return count;
+// }
+
+#include <iostream>
+using namespace std;
+
+int temp[100000];
+
+long int join(int s[], int left, int mid, int right)
+{
+    long int shift=0;
+    int i=left, j=mid, k=left;
+    while(i<mid && j<=right)
+    {
+        if(s[i] <= s[j]){
+            temp[k]=s[i];
+            k++, i++;
+        }
+        else{
+            temp[k]=s[j];
+            k++, j++;
+            shift += mid-i;
+        }
     }
-    return count;
+    while(i<mid){
+        temp[k] = s[i];
+        k++, i++;
+    }
+    
+    while(j<=right){
+        temp[k] = s[j];
+        k++, j++;
+    }
+    
+    while(left<=right){
+        s[left] = temp[left];
+        left++;
+    }
+    return shift;
+}
+
+long int mergeSort(int s[], int left, int right)
+{
+    long int shift = 0;
+    if(left < right)
+    {
+        int mid = left + (right-left)/2;
+        shift += mergeSort(s, left, mid);
+        shift += mergeSort(s, mid+1, right);
+        shift += join(s, left, mid+1, right);
+    }
+    return shift;
+}
+
+int main()
+{
+    int n, t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        int s[n];
+        for(int i=0; i<n; i++)
+            cin >> s[i];
+         
+        long int shift = mergeSort(s, 0, n-1);
+        
+        cout << shift << endl;
+    }
+    return 0;
 }
