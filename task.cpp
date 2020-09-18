997c43d1a17f6aedaff953c42928e55c140b83bd
aaqib925 <muhammadaaqib925@gmail.com>
2020-09-18 18:41:20 +0500

    working

--- task.cpp
+++ task.cpp
@@ -5491,3 +5491,99 @@ using namespace std;
 //     return sum;
 
 // }
+
+// struct Node { 
+//     int data; 
+//     struct Node* next; 
+//     Node(int data) 
+//     { 
+//         this->data = data; 
+//         next = NULL; 
+//     } 
+// }; 
+  
+// struct LinkedList { 
+//     Node* head; 
+//     LinkedList() 
+//     { 
+//         head = NULL; 
+//     } 
+  
+//     /* Function to reverse the linked list */
+//     void reverse() 
+//     { 
+//         // Initialize current, previous and 
+//         // next pointers 
+//         Node* current = head; 
+//         Node *prev = NULL, *next = NULL; 
+  
+//         while (current != NULL) { 
+//             // Store next 
+//             next = current->next; 
+  
+//             // Reverse current node's pointer 
+//             current->next = prev; 
+  
+//             // Move pointers one position ahead. 
+//             prev = current; 
+//             current = next; 
+//         } 
+//         head = prev; 
+//     } 
+  
+//     /* Function to print linked list */
+//     void print() 
+//     { 
+//         struct Node* temp = head; 
+//         while (temp != NULL) { 
+//             cout << temp->data << " "; 
+//             temp = temp->next; 
+//         } 
+//     } 
+  
+//     void push(int data) 
+//     { 
+//         Node* temp = new Node(data); 
+//         temp->next = head; 
+//         head = temp; 
+//     } 
+// }; 
+  
+// /* Driver program to test above function*/
+// int main() 
+// { 
+//     /* Start with the empty list */
+//     LinkedList ll; 
+//     ll.push(20); 
+//     ll.push(4); 
+//     ll.push(15); 
+//     ll.push(85); 
+  
+//     cout << "Given linked list\n"; 
+//     ll.print(); 
+  
+//     ll.reverse(); 
+  
+//     cout << "\nReversed Linked list \n"; 
+//     ll.print(); 
+//     return 0; 
+// } 
+
+
+// Function ListNode reverse(ListNode head)
+// {
+//     ListNode prev = null;
+//     ListNode curr = head;
+//     ListNode next = null;
+
+//     while(curr != null)
+//     {
+//         next = curr.next; // saves next
+//         curr.next = prev; // reverse
+//         prev = curr;
+//         curr = next;
+//     }
+
+//     return prev;    // nre Reversed List
+// }
+
