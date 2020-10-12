47337644406032a8fda8405968d04e0b2fde24a2
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-12 10:31:47 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7046,27 +7046,27 @@ void sumOddLengthSubarrays(vector<int> arr)
 //     countGoodTriplets({5,5,2,6,4}, 5,4,5);
 // }
 
-class Solution
-{
-public:
-    int countGoodTriplets(vector<int> &arr, int a, int b, int c)
-    {
-        int res = 0;
-        int n = arr.size();
-        for (int i = 0; i <= n - 3; i++)
-        {
-            for (int j = i + 1; j <= n - 2; j++)
-            {
-                if (abs(arr[j] - arr[i]) > a)
-                    continue;
-                for (int k = j + 1; k <= n - 1; k++)
-                {
-                    if (abs(arr[k] - arr[j]) > b || abs(arr[k] - arr[i]) > c)
-                        continue;
-                    res++;
-                }
-            }
-        }
-        return res;
-    }
-};
+// class Solution
+// {
+// public:
+//     int countGoodTriplets(vector<int> &arr, int a, int b, int c)
+//     {
+//         int res = 0;
+//         int n = arr.size();
+//         for (int i = 0; i <= n - 3; i++)
+//         {
+//             for (int j = i + 1; j <= n - 2; j++)
+//             {
+//                 if (abs(arr[j] - arr[i]) > a)
+//                     continue;
+//                 for (int k = j + 1; k <= n - 1; k++)
+//                 {
+//                     if (abs(arr[k] - arr[j]) > b || abs(arr[k] - arr[i]) > c)
+//                         continue;
+//                     res++;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
