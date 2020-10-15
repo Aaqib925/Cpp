81adb7b4fba641e93c953b324b1942a31ee4652d
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-15 15:40:18 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7081,18 +7081,18 @@ void sumOddLengthSubarrays(vector<int> arr)
 //     }
 // };
 
-class Solution {
-public:
-    int diagonalSum(vector<vector<int>>& mat) {
-        int sum = 0;
-        for (int i = 0; i < mat.size(); i++)
-        {
-            for (int j = 0; j < mat[0].size(); j++)
-            {
-                if (i == j || i + j == mat[0].size() - 1)
-                    sum += mat[i][j];
-            }
-        }
-        return sum;
-    }
-};
+// class Solution {
+// public:
+//     int diagonalSum(vector<vector<int>>& mat) {
+//         int sum = 0;
+//         for (int i = 0; i < mat.size(); i++)
+//         {
+//             for (int j = 0; j < mat[0].size(); j++)
+//             {
+//                 if (i == j || i + j == mat[0].size() - 1)
+//                     sum += mat[i][j];
+//             }
+//         }
+//         return sum;
+//     }
+// };
