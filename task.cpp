8f91d934afb07f65665578dfb08108224fa00d81
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-20 00:24:09 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7167,11 +7167,22 @@ void sumOddLengthSubarrays(vector<int> arr)
 
 int maximum69Number(int num)
 {
-    
+    string x = to_string(num);
+    for (int i = 0; i < x.length(); i++)
+    {
+        if (x[i] == '6')
+        {
+            x[i] = '9';
+            break;
+        }
+    }
+    stringstream geek(x);
+    geek >> num;
+    return num;
 }
 
 
 int main()
 {
-    cout << maximum69Number(9669) << endl;
+    cout << maximum69Number(9999) << endl;
 }
