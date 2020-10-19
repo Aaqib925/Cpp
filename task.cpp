0d986477307a01b03bd97f913c486f930c3d2cae
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-20 01:16:41 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7199,13 +7199,13 @@ void sumOddLengthSubarrays(vector<int> arr)
 //     }
 // };
 
-class Solution
-{
-public:
-    int maxProduct(vector<int> &nums)
-    {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return (nums[n - 1] - 1) * (nums[n - 2] - 1);
-    }
-};
+// class Solution
+// {
+// public:
+//     int maxProduct(vector<int> &nums)
+//     {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+//     }
+// };
