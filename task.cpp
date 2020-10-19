1fd508269a605fb26ec199c8c20d59343d768957
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-20 00:42:53 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7186,3 +7186,16 @@ void sumOddLengthSubarrays(vector<int> arr)
 // {
 //     cout << maximum69Number(9999) << endl;
 // }
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0; i < endTime.size(); i++)
+        {
+            if (startTime[i]<=queryTime && endTime[i]>=queryTime)
+                count ++;
+        }
+        return count;
+    }
+};
