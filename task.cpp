fc001ed4c63b0f29ddc2c10fe891167bed6ddcff
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-26 20:24:33 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7401,3 +7401,17 @@ void sumOddLengthSubarrays(vector<int> arr)
 //         return ans;
 //     }
 // };
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            A[i] = A[i] * A[i];
+        }
+        sort(A.begin(), A.end());
+        return A;
+    }
+};
