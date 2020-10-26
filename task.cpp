d5e0103b7e6b8f7cfae50fb7df345336f7271788
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-26 19:20:35 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7385,3 +7385,19 @@ void sumOddLengthSubarrays(vector<int> arr)
 //         return count;
 //     }
 // };
+
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 1; i + 2)
+        {
+
+            ans += min(nums[i], nums[i + 1]);
+        }
+        return ans;
+    }
+};
