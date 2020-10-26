c2e33f78fa48f51b307631f67e673c492f187c5a
aaqib925 <muhammadaaqib925@gmail.com>
2020-10-26 21:10:10 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7472,18 +7472,18 @@ void sumOddLengthSubarrays(vector<int> arr)
 //     }
 // };
 
-class Solution
-{
-public:
-    bool canMakeArithmeticProgression(vector<int> &arr)
-    {
-        sort(arr.begin(), arr.end());
-        int diff = arr[1] - arr[0];
-        for (int i = 1; i < arr.size(); i++)
-        {
-            if (arr[i] - arr[i - 1] != diff)
-                return false;
-        }
-        return true;
-    }
-};
+// class Solution
+// {
+// public:
+//     bool canMakeArithmeticProgression(vector<int> &arr)
+//     {
+//         sort(arr.begin(), arr.end());
+//         int diff = arr[1] - arr[0];
+//         for (int i = 1; i < arr.size(); i++)
+//         {
+//             if (arr[i] - arr[i - 1] != diff)
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
