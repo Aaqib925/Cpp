df348d1c160fe2fe100243acb71dd8f03c031e9d
aaqib925 <muhammadaaqib925@gmail.com>
2020-11-02 15:26:09 +0500

    working

--- task.cpp
+++ task.cpp
@@ -7840,14 +7840,14 @@ void printMultiVector(vector<vector<int>> x)
 //     }
 // };
 
-class Solution
-{
-public:
-    int countOdds(int low, int high)
-    {
-        if (low % 2 == 1)
-            return (high - low) / 2 + 1;
-        else
-            return (high - low + 1) / 2;
-    }
-};
+// class Solution
+// {
+// public:
+//     int countOdds(int low, int high)
+//     {
+//         if (low % 2 == 1)
+//             return (high - low) / 2 + 1;
+//         else
+//             return (high - low + 1) / 2;
+//     }
+// };
