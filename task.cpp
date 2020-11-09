52d94d6353a14ce4d36fcdae5b8bcdf3900cb3d0
aaqib925 <muhammadaaqib925@gmail.com>
2020-11-09 19:19:11 +0500

    working

--- task.cpp
+++ task.cpp
@@ -8073,23 +8073,23 @@ void printMultiVector(vector<vector<int>> x)
 //         return false;
 //     }
 // };
-bool isUgly(int num)
-{
-    if (num == 0)
-        return false;
-    while (num % 2 == 0)
-    {
-        num = num / 2;
-    }
-    while (num % 3 == 0)
-    {
-        num = num / 3;
-    }
-    while (num % 5 == 0)
-    {
-        num = num / 5;
-    }
-    if (num == 1)
-        return true;
-    return false;
-}
+// bool isUgly(int num)
+// {
+//     if (num == 0)
+//         return false;
+//     while (num % 2 == 0)
+//     {
+//         num = num / 2;
+//     }
+//     while (num % 3 == 0)
+//     {
+//         num = num / 3;
+//     }
+//     while (num % 5 == 0)
+//     {
+//         num = num / 5;
+//     }
+//     if (num == 1)
+//         return true;
+//     return false;
+// }
