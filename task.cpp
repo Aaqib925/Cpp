56194d073a2ef57827c49b61a08a3fec5337a0ac
aaqib925 <muhammadaaqib925@gmail.com>
2020-11-18 08:57:09 +0500

    working

--- task.cpp
+++ task.cpp
@@ -8316,3 +8316,29 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int ans = -1;
+		int n = nums.size();
+
+		for(int i=0;i<=n;i++){
+
+			int count = 0;
+			int x = i;
+
+			for(int j=0;j<n;j++)
+				if(nums[j] >= x)
+                    count++;
+
+			if(count == x){
+				ans = x;
+				break;
+			}
+
+		}
+
+		return ans;
+    }
+};
