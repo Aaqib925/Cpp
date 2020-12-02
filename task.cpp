f89b8d229458f24d4e2ffda4d3a9239b4f4e06de
aaqib925 <muhammadaaqib925@gmail.com>
2020-12-02 17:20:14 +0500

    working

--- task.cpp
+++ task.cpp
@@ -8915,22 +8915,22 @@ void printMultiVector(vector<vector<int>> x)
 //     }
 //     cout << count << endl;
 // }
-class Solution {
-public:
-    int minimumDeviation(vector<int>& nums) {
-        int res = INT_MAX, min_n = INT_MAX;
-        priority_queue<int> pq;
-        for (auto n : nums) {
-            n = n % 2 ? n * 2 : n;
-            pq.push(n);
-            min_n = min(min_n, n); 
-        }
-        while (pq.top() % 2 == 0) {
-            res = min(res, pq.top() - min_n);
-            min_n = min(min_n, pq.top() / 2);
-            pq.push(pq.top() / 2);
-            pq.pop();
-        }
-        return min(res, pq.top() - min_n);
-    }
-};
+// class Solution {
+// public:
+//     int minimumDeviation(vector<int>& nums) {
+//         int res = INT_MAX, min_n = INT_MAX;
+//         priority_queue<int> pq;
+//         for (auto n : nums) {
+//             n = n % 2 ? n * 2 : n;
+//             pq.push(n);
+//             min_n = min(min_n, n); 
+//         }
+//         while (pq.top() % 2 == 0) {
+//             res = min(res, pq.top() - min_n);
+//             min_n = min(min_n, pq.top() / 2);
+//             pq.push(pq.top() / 2);
+//             pq.pop();
+//         }
+//         return min(res, pq.top() - min_n);
+//     }
+// };
