c2b8439272d5b61d4f540ff7838abaca7ac54649
aaqib925 <muhammadaaqib925@gmail.com>
2021-01-13 15:45:24 +0500

    working

--- task.cpp
+++ task.cpp
@@ -10329,20 +10329,20 @@ int INDEX(string T, string P)
     
 // }
 
-class Solution {
-public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
-        int i = 0, j = people.size() - 1;
-        int ans = 0;
+// class Solution {
+// public:
+//     int numRescueBoats(vector<int>& people, int limit) {
+//         sort(people.begin(), people.end());
+//         int i = 0, j = people.size() - 1;
+//         int ans = 0;
 
-        while (i <= j) {
-            ans++;
-            if (people[i] + people[j] <= limit)
-                i++;
-            j--;
-        }
+//         while (i <= j) {
+//             ans++;
+//             if (people[i] + people[j] <= limit)
+//                 i++;
+//             j--;
+//         }
 
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
